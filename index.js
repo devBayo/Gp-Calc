7b07b84b7e3c00066113845517a4dfb74326e923
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-12-02 16:26:24 +0100

    feat: added calculate functionality

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
-const addBtn = document.querySelector(".add-course");
+const addBtn = document.querySelector('.add-course');
+const btnClear = document.querySelector('.remove-course');
+const courseContainer = document.querySelector('.inp-group');
+const btnCalculate = document.querySelector('.btn-calculate');
+const gpaEl = document.querySelector('.gpa-value');
 
-const removeBtn = document.querySelector(".remove-course");
+const gradeMapper = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 0,
+};
 
-const input = document.querySelector(".inp-group");
+function addCourse() {
+  const html = `
+    <div class="flex">
+      <input type="text" placeholder="Eg. Advanced Calculus" />
+      <select class="grade">
+        <option value="">Select</option>
+        <option value="A">A</option>
+        <option value="B">B</option>
+        <option value="C">C</option>
+        <option value="D">D</option>
+        <option value="E">E</option>
+        <option value="F">F</option>
+      </select>
+      <input type="number" class="score" placeholder="Eg. 20" />
+      <button type="button" class="delete">&times;</button>
+    </div>
+    `;
 
-function removeInput() {
-  this.parentElement.remove();
+  courseContainer.insertAdjacentHTML('beforeend', html);
 }
 
-function addInput() {
-  const name = document.createElement("input");
-  name.type = "text";
-  name.placeholder = "Eg. Advanced Calculus";
-
-  const values = ["Select", "A", "B", "C", "D", "E", "F"];
-
-  const select = document.createElement("select");
-  select.type = "select";
-  select.placeholder = "select";
-
-  for (let i = 0; i < values.length; i++) {
-    let option = document.createElement("option");
-    option.value = values[i];
-    option.innerHTML = values[i];
-    select.appendChild(option);
-  }
-  const credit = document.createElement("input");
-  credit.type = "text";
-  credit.placeholder = "Eg. 20";
-
-  const btn = document.createElement("a");
-  btn.className = "delete";
-  btn.innerHTML = "&times";
-
-  btn.addEventListener("click", removeInput);
-
-  const flex = document.createElement("div");
-  flex.className = "flex";
-
-  input.appendChild(flex);
-  flex.appendChild(name);
-  flex.appendChild(select);
-  flex.appendChild(credit);
-  flex.appendChild(btn);
+function removeCourse(e) {
+  if (!e.target.classList.contains('delete')) return;
+  e.target.parentElement.remove();
 }
 
-function clearThis() {
-  input.innerHTML = "";
+function clearAll() {
+  courseContainer.innerHTML = '';
 }
 
-addBtn.addEventListener("click", addInput);
-removeBtn.addEventListener("click", clearThis);
+addBtn.addEventListener('click', addCourse);
+btnClear.addEventListener('click', clearAll);
+courseContainer.addEventListener('click', removeCourse);
+
+function calculate() {
+  const gradesEl = document.querySelectorAll('.grade');
+  const grades = [...gradesEl].map(el => gradeMapper[el.value]);
+
+  const creditsEl = document.querySelectorAll('.score');
+  const credits = [...creditsEl].map(el => +el.value);
+  const totalGrades = credits.reduce((prev, cur) => prev + cur, 0);
+
+  const grade_X_credit = credits.map((credit, i) => credit * grades[i]);
+  const total_grade_X_credit = grade_X_credit.reduce(
+    (prev, cur) => prev + cur,
+    0
+  );
+  const gpa = total_grade_X_credit / totalGrades;
+  gpaEl.textContent = gpa.toFixed(2);
+}
+
+btnCalculate.addEventListener('click', calculate);
